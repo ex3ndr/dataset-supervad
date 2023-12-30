c0d94fd1f4df1fc364d07fbf87d0fc515f08955e
Steve Korshakov <steve@korshakov.com>
2023-12-30 13:59:04 -0800

    fix: fix packing of non speech

--- pack.sh
+++ pack.sh
@@ -7,6 +7,7 @@ mkdir -p $DATASETS_PATH/archive
 # Archive
 tar --use-compress-program="pigz --best --recursive" -czvf $DATASETS_PATH/archive/rir_real.tar.gz -C $DATASETS_PATH/output rir_real
 tar --use-compress-program="pigz --best --recursive" -czvf $DATASETS_PATH/archive/rir_synthetic.tar.gz -C $DATASETS_PATH/output rir_synthetic
+tar --use-compress-program="pigz --best --recursive" -czvf $DATASETS_PATH/archive/non_speech.tar.gz -C $DATASETS_PATH/output non_speech
 tar --use-compress-program="pigz --best --recursive" -czvf $DATASETS_PATH/archive/speech_train.tar.gz -C $DATASETS_PATH/output speech_train
 tar --use-compress-program="pigz --best --recursive" -czvf $DATASETS_PATH/archive/speech_test.tar.gz -C $DATASETS_PATH/output speech_test
 tar --use-compress-program="pigz --best --recursive" -czvf $DATASETS_PATH/archive/vad_train.tar.gz -C $DATASETS_PATH/output vad_train
